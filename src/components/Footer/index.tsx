57e886de38c5e2bc8711c53654914e989b4e3b9c
Ederson Franzen Fagundes <ederson@bemobile.tech>
2023-04-02 15:38:21 -0300

    feat: footer component created

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <section className="p-4 text-center">
+      Feito com ❤️ por{" "}
+      <a className="text-blue-700" href="https://github.com/edersonff">
+        Ederson Franzen Fagundes{" "}
+      </a>
+    </section>
+  );
+}
